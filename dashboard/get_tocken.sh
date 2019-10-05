3abed235a9ea1b1cbb3987dd9660814920e1413d
github-cyf <1627999813@qq.com>
2019-10-05 17:07:58 +0800

    完成部署heapster部署

--- dashboard/get_tocken.sh
+++ dashboard/get_tocken.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret |grep 'dashboard-token' |awk '{print $1}')
